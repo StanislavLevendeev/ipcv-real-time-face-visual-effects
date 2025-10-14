68736620d0c380a9120f9fee1adfbf41f6b2bb67
SLevendeev <stanislav-levendeev@mail.ru>
2025-10-14 12:08:16 +0200

    Add face detection to motion tracking process

--- tasks/motion_tracking.py
+++ tasks/motion_tracking.py
@@ -1,10 +1,16 @@
 import cv2 as cv
+from utils.face_recognition import detect_face
 
 
 class MotionTracking:
     def process_frame(self, frame):
         # Your implementation comes here:
-
+        faces = detect_face(frame)
+        for x, y, w, h in faces:
+            center = (x + w // 2, y + h // 2)
+            frame = cv.ellipse(
+                frame, center, (w // 2, h // 2), 0, 0, 360, (255, 0, 255), 4
+            )
         return frame
 
     def display_label(self, frame):
