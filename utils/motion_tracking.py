ccd894af4095f2bb6ebe23206fe0371b91c9b79c
SLevendeev <stanislav-levendeev@mail.ru>
2025-10-20 14:44:58 +0200

    Cleaned up the file and added gesture detection function

--- utils/motion_tracking.py
+++ utils/motion_tracking.py
@@ -1,75 +1,59 @@
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-from mediapipe import solutions
-from mediapipe.framework.formats import landmark_pb2
-import numpy as np
-import cv2 as cv
+from utils.display.hand_display import draw_landmarks_on_image
+from utils.display.gesture_display import draw_gesture_on_image
+import threading
 
-base_options = python.BaseOptions(model_asset_path="hand_landmarker.task")
-options = vision.HandLandmarkerOptions(base_options=base_options, num_hands=2)
-detector = vision.HandLandmarker.create_from_options(options)
-MARGIN = 10  # pixels
-FONT_SIZE = 1
-FONT_THICKNESS = 1
-HANDEDNESS_TEXT_COLOR = (88, 205, 54)  # vibrant green
+# A global variable to store last result safely
+last_gesture_result = None
+lock = threading.Lock()
+base_hand_options = python.BaseOptions(model_asset_path="hand_landmarker.task")
+hand_options = vision.HandLandmarkerOptions(base_options=base_hand_options, num_hands=2)
+hand_detector = vision.HandLandmarker.create_from_options(hand_options)
+
+DESIRED_HEIGHT = 480
+DESIRED_WIDTH = 480
+
+timestamp = 0
+
+
+def print_result(result, output_image, timestamp_ms):
+    global last_gesture_result
+    with lock:
+        last_gesture_result = result  # save the latest async result
+
+
+base_gesture_options = python.BaseOptions(model_asset_path="gesture_recognizer.task")
+options_gesture = vision.GestureRecognizerOptions(
+    base_options=base_gesture_options,
+    min_hand_detection_confidence=0.1,
+    min_hand_presence_confidence=0.1,
+    num_hands=2,
+    min_tracking_confidence=0.3,
+    running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
+    result_callback=print_result,
+)
+recognizer_gesture = vision.GestureRecognizer.create_from_options(options_gesture)
 
 
 def track_hand(frame):
     # STEP 4: Detect hand landmarks from the input image.
     image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
-    detection_result = detector.detect(image)
+    detection_result = hand_detector.detect(image)
 
     # STEP 5: Process the classification result. In this case, visualize it.
     annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
     return annotated_image
 
 
-def draw_landmarks_on_image(rgb_image, detection_result):
-    hand_landmarks_list = detection_result.hand_landmarks
-    handedness_list = detection_result.handedness
-    annotated_image = np.copy(rgb_image)
+def track_gesture(frame):
+    global timestamp
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    recognizer_gesture.recognize_async(image, timestamp)
 
-    # Loop through the detected hands to visualize.
-    for idx in range(len(hand_landmarks_list)):
-        hand_landmarks = hand_landmarks_list[idx]
-        handedness = handedness_list[idx]
-
-        # Draw the hand landmarks.
-        hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
-        hand_landmarks_proto.landmark.extend(
-            [
-                landmark_pb2.NormalizedLandmark(
-                    x=landmark.x, y=landmark.y, z=landmark.z
-                )
-                for landmark in hand_landmarks
-            ]
-        )
-        solutions.drawing_utils.draw_landmarks(
-            annotated_image,
-            hand_landmarks_proto,
-            solutions.hands.HAND_CONNECTIONS,
-            solutions.drawing_styles.get_default_hand_landmarks_style(),
-            solutions.drawing_styles.get_default_hand_connections_style(),
-        )
-
-        # Get the top left corner of the detected hand's bounding box.
-        height, width, _ = annotated_image.shape
-        x_coordinates = [landmark.x for landmark in hand_landmarks]
-        y_coordinates = [landmark.y for landmark in hand_landmarks]
-        text_x = int(min(x_coordinates) * width)
-        text_y = int(min(y_coordinates) * height) - MARGIN
-
-        # Draw handedness (left or right hand) on the image.
-        cv.putText(
-            annotated_image,
-            f"{handedness[0].category_name}",
-            (text_x, text_y),
-            cv.FONT_HERSHEY_DUPLEX,
-            FONT_SIZE,
-            HANDEDNESS_TEXT_COLOR,
-            FONT_THICKNESS,
-            cv.LINE_AA,
-        )
-
-    return annotated_image
+    timestamp += 33  # approximate 30 FPS
+    with lock:
+        if last_gesture_result:
+            frame = draw_gesture_on_image(frame, last_gesture_result)
+    return frame
