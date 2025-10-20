a593af5bfb520bbfda3a820962055cb2cf3e5019
SLevendeev <stanislav-levendeev@mail.ru>
2025-10-20 16:14:37 +0200

    Enhance gesture tracking by adding gesture recognition and handling for "Pointing_Up"

--- tasks/motion_tracking.py
+++ tasks/motion_tracking.py
@@ -1,12 +1,14 @@
 import cv2 as cv
 from utils.face_recognition import detect_face
-from utils.motion_tracking import track_hand, track_gesture
+from utils.motion_tracking import track_hand, track_gesture, is_wanted_gesture
 
 
 class MotionTracking:
     def process_frame(self, frame):
         # Your implementation comes here:
-        frame = track_gesture(frame)
+        frame, result = track_gesture(frame)
+        if is_wanted_gesture(result, "Pointing_Up"):
+            print("You are pointing up!")
         faces = detect_face(frame)
         for x, y, w, h in faces:
             center = (x + w // 2, y + h // 2)
--- utils/motion_tracking.py
+++ utils/motion_tracking.py
@@ -4,11 +4,25 @@ from mediapipe.tasks.python import vision
 from utils.display.hand_display import draw_landmarks_on_image
 from utils.display.gesture_display import draw_gesture_on_image
 import threading
+import os
 
 # A global variable to store last result safely
 last_gesture_result = None
 lock = threading.Lock()
-base_hand_options = python.BaseOptions(model_asset_path="hand_landmarker.task")
+
+hand_marker_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), "..", "data", "hand_landmarker.task"
+    )
+)
+
+gesture_recognizer_path = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), "..", "data", "gesture_recognizer.task"
+    )
+)
+
+base_hand_options = python.BaseOptions(model_asset_path=hand_marker_path)
 hand_options = vision.HandLandmarkerOptions(base_options=base_hand_options, num_hands=2)
 hand_detector = vision.HandLandmarker.create_from_options(hand_options)
 
@@ -24,13 +38,10 @@ def print_result(result, output_image, timestamp_ms):
         last_gesture_result = result  # save the latest async result
 
 
-base_gesture_options = python.BaseOptions(model_asset_path="gesture_recognizer.task")
+base_gesture_options = python.BaseOptions(model_asset_path=gesture_recognizer_path)
 options_gesture = vision.GestureRecognizerOptions(
     base_options=base_gesture_options,
-    min_hand_detection_confidence=0.1,
-    min_hand_presence_confidence=0.1,
     num_hands=2,
-    min_tracking_confidence=0.3,
     running_mode=mp.tasks.vision.RunningMode.LIVE_STREAM,
     result_callback=print_result,
 )
@@ -53,7 +64,22 @@ def track_gesture(frame):
     recognizer_gesture.recognize_async(image, timestamp)
 
     timestamp += 33  # approximate 30 FPS
+
+    gestures = []
     with lock:
         if last_gesture_result:
+            gestures = []
+            for hands in last_gesture_result.gestures:
+                gestures.append(hands[0].category_name)
             frame = draw_gesture_on_image(frame, last_gesture_result)
-    return frame
+    return frame, last_gesture_result
+
+
+def is_wanted_gesture(result, category):
+    if result is None:
+        return False
+    gestures = []
+
+    for hands in result.gestures:
+        gestures.append(hands[0].category_name)
+    return category in gestures
